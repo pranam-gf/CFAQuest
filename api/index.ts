f6dba1b272733550be50fd3939f3e99b8a80bcb8
pranam-gf <pranam@goodfin.com>
2025-08-02 14:38:29 -0400

    vercel fix

--- api/index.ts
+++ api/index.ts
@@ -1,6 +1,14 @@
 // This file serves as the entry point for the Vercel serverless function.
 // It imports the main server logic from the /server directory.
 
-import app from '../server/index';
+import { setupApp } from '../server/index.ts';
 
-export default app;
+// Create and export the app instance for Vercel
+let appInstance: any = null;
+
+export default async (req: any, res: any) => {
+  if (!appInstance) {
+    appInstance = await setupApp();
+  }
+  return appInstance(req, res);
+};
